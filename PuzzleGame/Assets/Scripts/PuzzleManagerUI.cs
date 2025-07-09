818feee860fe25849c275792d7ec524ad96afead
Barış Erkut <bariserkutcm@gmail.com>
2025-07-09 03:59:40 +0300

    Win check

--- PuzzleGame/Assets/Scripts/PuzzleManagerUI.cs
+++ PuzzleGame/Assets/Scripts/PuzzleManagerUI.cs
@@ -1,54 +1,66 @@
+// PuzzleManagerUI.cs
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class PuzzleManagerUI : MonoBehaviour
 {
+    [Header("Puzzle Ayarları")]
     public Texture2D sourceTexture;
     public int rows = 5;
     public int columns = 10;
 
-    public GameObject piecePrefab;
-    public Transform gridParent;
+    [Header("Prefab & UI")]
+    public GameObject piecePrefab;  // İçinde Image + PuzzlePieceUI var
+    public Transform gridParent;    // GridLayoutGroup, Start Corner = Upper Left
 
     private List<PuzzlePieceUI> pieces = new List<PuzzlePieceUI>();
-    private PuzzlePieceUI firstSelected = null;
+    private PuzzlePieceUI firstSelected;
 
     void Start()
     {
-        SliceAndCreate();
-        Shuffle();
+        Debug.Log("▶️ CreatePieces başlatılıyor");
+        CreatePieces();
+        ShufflePieces();
     }
 
-    void SliceAndCreate()
+    void CreatePieces()
     {
+        // Önceki parçaları temizle
+        foreach (Transform child in gridParent) Destroy(child.gameObject);
         pieces.Clear();
 
-        int pieceWidth = sourceTexture.width / columns;
-        int pieceHeight = sourceTexture.height / rows;
+        int w = sourceTexture.width  / columns;
+        int h = sourceTexture.height / rows;
 
-        for (int i = 0; i < rows * columns; i++)
+        // Upper Left referanslı satır/sütun döngüsü
+        for (int row = 0; row < rows; row++)
         {
-            int x = i % columns;
-            int y = i / columns;
+            int invRow = (rows - 1) - row; // Texture2D alt-sol orijini düzeltmesi
+            for (int col = 0; col < columns; col++)
+            {
+                int id = row * columns + col; // 0..49
+                Debug.Log($"🌀 Loop: row={row}, col={col}, id={id}");
 
-            Rect rect = new Rect(x * pieceWidth, y * pieceHeight, pieceWidth, pieceHeight);
-            Sprite sprite = Sprite.Create(sourceTexture, rect, new Vector2(0.5f, 0.5f), 100f);
+                Rect rect = new Rect(col * w, invRow * h, w, h);
+                Sprite spr = Sprite.Create(sourceTexture, rect, new Vector2(0.5f, 0.5f), 100f);
 
-            GameObject obj = Instantiate(piecePrefab, gridParent);
-            PuzzlePieceUI piece = obj.GetComponent<PuzzlePieceUI>();
-            piece.Setup(this, sprite, i);
-
-            pieces.Add(piece);
+                GameObject go = Instantiate(piecePrefab, gridParent);
+                go.name = $"Piece_{id}";
+                PuzzlePieceUI piece = go.GetComponent<PuzzlePieceUI>();
+                piece.Setup(this, spr, id);
+                pieces.Add(piece);
+            }
         }
     }
 
-    void Shuffle()
+    void ShufflePieces()
     {
-        for (int i = 0; i < pieces.Count; i++)
+        var rnd = new System.Random();
+        for (int i = pieces.Count - 1; i > 0; i--)
         {
-            int rand = Random.Range(0, pieces.Count);
-            SwapSprites(pieces[i], pieces[rand]);
+            int j = rnd.Next(0, i + 1);
+            pieces[i].SwapWith(pieces[j]);
         }
     }
 
@@ -57,20 +69,32 @@ public class PuzzleManagerUI : MonoBehaviour
         if (firstSelected == null)
         {
             firstSelected = clicked;
-            clicked.image.color = Color.yellow; // seçili efekti
+            firstSelected.image.color = Color.yellow;
         }
         else
         {
-            SwapSprites(firstSelected, clicked);
+            clicked.image.color       = Color.white;
             firstSelected.image.color = Color.white;
+
+            firstSelected.SwapWith(clicked);
             firstSelected = null;
+
+            Debug.Log("Takas yapıldı, kontrol ediliyor...");
+            CheckForWin();
         }
     }
 
-    void SwapSprites(PuzzlePieceUI a, PuzzlePieceUI b)
+    void CheckForWin()
     {
-        Sprite temp = a.image.sprite;
-        a.image.sprite = b.image.sprite;
-        b.image.sprite = temp;
+        for (int i = 0; i < pieces.Count; i++)
+        {
+            if (!pieces[i].IsCorrect())
+            {
+                Debug.Log($"✖ Parça {i}: current={pieces[i].currentID}, correct={pieces[i].correctID}");
+                return;
+            }
+        }
+
+        Debug.Log("🎉 Kazandın!");
     }
 }
--- PuzzleGame/Assets/Scripts/PuzzlePieceUI.cs
+++ PuzzleGame/Assets/Scripts/PuzzlePieceUI.cs
@@ -1,23 +1,49 @@
+// PuzzlePieceUI.cs
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class PuzzlePieceUI : MonoBehaviour, IPointerClickHandler
 {
-    public int index; // Listenin içindeki konumu
-    public Image image;
+    [HideInInspector] public int correctID;
+    [HideInInspector] public int currentID;
+    [HideInInspector] public Image image;
     private PuzzleManagerUI manager;
 
-    public void Setup(PuzzleManagerUI mgr, Sprite sprite, int i)
+    // Çağıran taraf: Setup(this, sprite, id)
+    public void Setup(PuzzleManagerUI mgr, Sprite sprite, int id)
     {
-        manager = mgr;
-        index = i;
-        image = GetComponent<Image>();
+        manager    = mgr;
+        correctID  = id;
+        currentID  = id;
+        image      = GetComponent<Image>();
         image.sprite = sprite;
+
+        Debug.Log($"[Setup] {gameObject.name} → correctID={correctID}, currentID={currentID}");
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         manager.OnPieceClicked(this);
+    }
+
+    // İki parçayı hem sprite hem ID olarak takas eder
+    public void SwapWith(PuzzlePieceUI other)
+    {
+        // Sprite takas
+        var tmpSprite      = image.sprite;
+        image.sprite       = other.image.sprite;
+        other.image.sprite = tmpSprite;
+
+        // ID takas
+        var tmpID          = currentID;
+        currentID          = other.currentID;
+        other.currentID    = tmpID;
+    }
+
+    // Kazanma kontrolü
+    public bool IsCorrect()
+    {
+        return currentID == correctID;
     }
 }
